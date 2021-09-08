8727696e66368b0d9e807451d66cd3db56caa1f4
MrNewaz <saifur.saif@northsouth.edu>
2021-09-09 00:24:57 +0600

    init

--- first code.asm
+++ first code.asm
@@ -2,11 +2,16 @@
 .stack 100h
 
 .data
-    ;All data
+    ;All data 
+    num1 db ?
 
 .code
     ;all codes
-    main proc
+    main proc  
+        
+        mov bh, 4
+        mov bl, 5
+        add bh,bl
         
         mov ah , 4ch
         int 21h
