5785e275bd9924416ba99e68267d4e5f216d5f9e
MrNewaz <saifur.saif@northsouth.edu>
2021-09-09 00:17:49 +0600

    init

---
+++ first code.asm
@@ -0,0 +1,19 @@
+.model small
+.stack 100h
+
+.data
+    ;All data
+
+.code
+    ;all codes
+    main proc
+        
+        mov ah , 4ch
+        int 21h
+        
+    main endp
+        
+        
+        
+    
+end main
